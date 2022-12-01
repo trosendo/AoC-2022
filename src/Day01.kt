678173797fd368f8d32676c7c4d81e397e228078
Tomás Rosendo <tomasrosendo@gmail.com>
2022-12-01 10:40:48 +0000

    Day01p02 - First solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,19 +1,31 @@
 fun main() {
-    fun part1(input: List<String>) =
-        input.fold(listOf(emptyList<Int>())) { acc, value ->
-            acc.plusElement(value.toIntOrNull()?.run {
-                acc[acc.lastIndex].plus(this)
-            } ?: emptyList())
-        }.maxOfOrNull { it.sum() } ?: -1
+    fun getListForEachElf(input: List<String>): Set<List<Int>> {
+        val (acc, elfBasket) = input.fold(setOf<List<Int>>() to emptyList<Int>()) { (acc, elfBasket), value ->
+            value.toIntOrNull()?.run {
+                acc to (elfBasket + this)
+            } ?: (acc.plusElement(elfBasket) to emptyList())
+        }
 
-
-    fun part2(input: List<String>): Int {
-        return input.size
+        return acc.plusElement(elfBasket)
     }
+
+    fun part1(input: List<String>) =
+        getListForEachElf(input)
+            .maxOfOrNull { it.sum() } ?: -1
+
+
+    fun part2(input: List<String>) =
+        getListForEachElf(input)
+            .map { it.sum() }
+            .sortedDescending()
+            .take(3)
+            .sum()
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
