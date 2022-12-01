79b64524dd80aebd89ffe848a0fec4a05a77cee6
Tomás Rosendo <tomasrosendo@gmail.com>
2022-12-01 10:12:55 +0000

    Day01p01 - First solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,7 +1,11 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) =
+        input.fold(listOf(emptyList<Int>())) { acc, value ->
+            acc.plusElement(value.toIntOrNull()?.run {
+                acc[acc.lastIndex].plus(this)
+            } ?: emptyList())
+        }.maxOfOrNull { it.sum() } ?: -1
+
 
     fun part2(input: List<String>): Int {
         return input.size
@@ -9,7 +13,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
