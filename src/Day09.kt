ea66a6f6179dc131a73f884c10acf3eef8e66a43
Tomás Rosendo <tomasrosendo@gmail.com>
2022-12-09 14:38:48 +0000

    Day09 - Solution

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,11 +1,9 @@
 import java.lang.Exception
 
-// enum Direction with U for Up, D for Down, L for Left, R for Right
 enum class Direction(val code: Char) {
     UP('U'), DOWN('D'), LEFT('L'), RIGHT('R')
 }
 
-// data class Position with x and y coordinates
 data class Position(val x: Int, val y: Int) {
     fun move(direction: Direction) = this.move(direction.code)
 
@@ -39,10 +37,10 @@ data class Position(val x: Int, val y: Int) {
             xDiff == 0 && yDiff < 0 -> this.move(Direction.DOWN)
             xDiff > 0 && yDiff == 0 -> this.move(Direction.LEFT)
             xDiff < 0 && yDiff == 0 -> this.move(Direction.RIGHT)
-            xDiff > 0 && yDiff > 0 -> this.move(Direction.LEFT).move(Direction.UP)//this.x - 1 to this.y - 1
-            xDiff < 0 && yDiff > 0 -> this.move(Direction.RIGHT).move(Direction.UP)//this.x + 1 to this.y - 1
-            xDiff > 0 && yDiff < 0 -> this.move(Direction.LEFT).move(Direction.DOWN)//this.x - 1 to this.y + 1
-            xDiff < 0 && yDiff < 0 -> this.move(Direction.RIGHT).move(Direction.DOWN)//this.x + 1 to this.y + 1
+            xDiff > 0 && yDiff > 0 -> this.move(Direction.LEFT).move(Direction.UP)
+            xDiff < 0 && yDiff > 0 -> this.move(Direction.RIGHT).move(Direction.UP)
+            xDiff > 0 && yDiff < 0 -> this.move(Direction.LEFT).move(Direction.DOWN)
+            xDiff < 0 && yDiff < 0 -> this.move(Direction.RIGHT).move(Direction.DOWN)
             else -> {
                 throw Exception("This should not happen")
             }
@@ -51,42 +49,75 @@ data class Position(val x: Int, val y: Int) {
 }
 
 fun main() {
+    fun checkTailIsTouchingHead(currentTailPosition: Position, nextHeadPosition: Position): Position {
+        return if (!currentTailPosition.isTouching(nextHeadPosition)) {
+            currentTailPosition.moveToBeTouching(nextHeadPosition)
+        } else {
+            currentTailPosition
+        }
+    }
 
-    fun part1(input: List<String>): Int {
-        val positions = input.fold(
-            listOf(Position(0,0)) to listOf(Position(0,0))
-        ) { (headPositions, tailPositions), s ->
+    fun moveHeadAndTail(
+        headPositions: List<Position>,
+        tailPositions: List<Position>,
+        direction: String
+    ): Pair<List<Position>, List<Position>> {
+        val currentHeadPosition = headPositions.last()
+        val currentTailPosition = tailPositions.last()
+
+        val nextHeadPosition = currentHeadPosition.move(direction.first())
+        val nextTailPosition = checkTailIsTouchingHead(currentTailPosition, nextHeadPosition)
+
+        return headPositions + nextHeadPosition to tailPositions + nextTailPosition
+    }
+
+    fun processMoves(input: List<String>) = input.fold(
+        listOf(Position(0, 0)) to listOf(Position(0, 0))
+    ) { (headPositions, tailPositions), s ->
+        val (direction, amount) = s.split(" ")
+
+        (0 until amount.toInt()).fold(
+            headPositions to tailPositions
+        ) { (headPositions, tailPositions), _ ->
+            moveHeadAndTail(headPositions, tailPositions, direction)
+        }.let { newPositions ->
+            headPositions + newPositions.first.takeLast(amount.toInt()) to
+                    tailPositions + newPositions.second.takeLast(amount.toInt())
+        }
+    }
+
+    fun processMovesV2(input: List<String>, numberOfKnots: Int): Int {
+        val listOfPositionsOfKnots = MutableList(numberOfKnots) { mutableListOf(Position(0, 0)) }
+        for(s in input) {
             val (direction, amount) = s.split(" ")
+            for (i in 0 until amount.toInt()) {
 
-            (0 until amount.toInt()).fold(
-                headPositions to tailPositions
-            ) { (headPositions, tailPositions), _ ->
-                val currentHeadPosition = headPositions.last()
-                val currentTailPosition = tailPositions.last()
+                val (newHeadPositions, newTailPositions) = moveHeadAndTail(listOfPositionsOfKnots[0], listOfPositionsOfKnots[1], direction)
 
-                //println("Current head position $currentHeadPosition moving $direction...")
-                val nextHeadPosition = currentHeadPosition.move(direction.first())
-                //println("Current head position $currentHeadPosition moved to $nextHeadPosition...")
-                val nextTailPosition = if(!currentTailPosition.isTouching(nextHeadPosition)) {
-                    //println("Tail is not touching head, moving tail to be touching head... Tail: $currentTailPosition, Head: $nextHeadPosition")
-                    currentTailPosition.moveToBeTouching(nextHeadPosition)
-                } else {
-                    currentTailPosition
+                listOfPositionsOfKnots[0] = newHeadPositions.toMutableList()
+                listOfPositionsOfKnots[1] = newTailPositions.toMutableList()
+
+                for(knot in 1 until listOfPositionsOfKnots.size - 1) {
+                    val headPositions = listOfPositionsOfKnots[knot]
+                    val tailPositions = listOfPositionsOfKnots[knot + 1]
+
+                    val nextTailPosition = checkTailIsTouchingHead(tailPositions.last(), headPositions.last())
+
+                    listOfPositionsOfKnots[knot + 1] += nextTailPosition
                 }
-
-                headPositions + nextHeadPosition to tailPositions + nextTailPosition
-            }.let { newPositions ->
-                headPositions + newPositions.first.takeLast(amount.toInt()) to
-                        tailPositions + newPositions.second.takeLast(amount.toInt())
             }
         }
 
+        return listOfPositionsOfKnots.last().distinct().size
+    }
+
+    fun part1(input: List<String>): Int {
+        val positions = processMoves(input)
         return positions.second.distinct().size
     }
 
     fun part2(input: List<String>): Int {
-
-        return -1
+        return processMovesV2(input, 10)
     }
 
     println("Result of Part01 (test): " + part1(readInputAsList("Day09_test")))
